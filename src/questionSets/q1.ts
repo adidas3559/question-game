c50847cb02accf8a6ceffb2e1fe16c6564c61585
Stephen Forbes <s.forbes@builderdesigns.com>
2026-09-19 22:01:27 -0500

    added emojis

--- src/questionSets/q1.ts
+++ src/questionSets/q1.ts
@@ -5,52 +5,52 @@ const q1: QuestionSet = {
     {
       id: "q1-1",
       question: "What is your favorite time of day?",
-      answers: ["Morning", "Afternoon", "Evening", "Late night"],
+      answers: ["Morning 🌅", "Afternoon 🌤️", "Evening 🌆", "Late night 🌙"],
     },
     {
       id: "q1-2",
       question: "Which would you rather have?",
-      answers: ["Fame", "Wealth", "Intelligence", "Good looks"],
+      answers: ["Fame 🌟", "Wealth 💰", "Intelligence 🧠", "Good looks 😎"],
     },
     {
       id: "q1-3",
       question: "What is your favorite type of weather?",
-      answers: ["Sunny", "Rainy", "Snowy", "Stormy"],
+      answers: ["Sunny ☀️", "Rainy 🌧️", "Snowy ❄️", "Stormy ⛈️"],
     },
     {
       id: "q1-4",
       question: "Which drink do you prefer?",
-      answers: ["Soda", "Coffee", "Tea", "Water"],
+      answers: ["Soda 🥤", "Coffee ☕", "Hot Chocolate 🍫", "Milk 🥛"],
     },
     {
       id: "q1-5",
       question: "What is your biggest pet peeve?",
-      answers: ["People being late", "Loud chewing", "Bad manners", "Being interrupted"],
+      answers: ["People being late ⏰", "Loud chewing 😖", "Bad manners 🙄", "Being interrupted 🗣️"],
     },
     {
       id: "q1-6",
       question: "Which would you rather be?",
-      answers: ["Extremely funny", "Extremely attractive", "Extremely smart", "Extremely talented"],
+      answers: ["Extremely funny 😂", "Extremely attractive 😍", "Extremely smart 🤓", "Extremely talented 🎭"],
     },
     {
       id: "q1-7",
       question: "What is your preferred way to spend a day off?",
-      answers: ["Sleeping in", "Being productive", "Going out", "Doing absolutely nothing"],
+      answers: ["Sleeping in 😴", "Being productive ✅", "Going out 🚗", "Doing absolutely nothing 🛋️"],
     },
     {
       id: "q1-8",
       question: "Which type of music do you listen to most?",
-      answers: ["Pop", "Hip-hop/Rap", "Rock", "Country"],
+      answers: ["Pop 🎤", "Hip-hop/Rap 🎧", "Rock 🎸", "Classical 🎻"],
     },
     {
       id: "q1-9",
       question: "What would you be most likely to buy if you suddenly got $500?",
-      answers: ["Clothes", "Electronics", "Food/going out", "Save it"],
+      answers: ["Clothes 👕", "Electronics 📱", "Food/going out 🍽️", "Save it 🏦"],
     },
     {
       id: "q1-10",
       question: "Which chore do you dislike the most?",
-      answers: ["Doing dishes", "Laundry", "Cleaning the bathroom", "Vacuuming"],
+      answers: ["Doing dishes 🧽", "Laundry 🧺", "Cleaning the bathroom 🚽", "Yard Work 🌿"],
     },
   ],
 }
